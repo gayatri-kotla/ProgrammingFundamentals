b10454a081d7ed1a66686f1d63843a07910e7a9e
gayatri-kotla <147698576+gayatri-kotla@users.noreply.github.com>
2023-10-31 12:49:51 +0530

    AdminDepartment.java

--- Assignment/src/com/gradedproject/q1/AdminDepartment.java
+++ Assignment/src/com/gradedproject/q1/AdminDepartment.java
@@ -2,17 +2,20 @@ package com.gradedproject.q1;
 
 public class AdminDepartment  extends SuperDepartment {
 	//String res = super.isTodayAHoliday();
-	public String departmentName() {
+	public String departmentName()
+	{
 		String str = "Admin Department";
 		return str;
 	}
 
-	public String getTodaysWork() {
+	public String getTodaysWork()
+	{
 		String str = "Complete your documents submission";
 		return str;
 	}
 
-	public String getWorkDeadline() {
+	public String getWorkDeadline() 
+	{
 		String str = "Complete by EOD";
 		return str;
 	}
