d3b77c34620258356c194e7153e58fa09270b798
gayatri-kotla <147698576+gayatri-kotla@users.noreply.github.com>
2023-10-31 12:50:41 +0530

    HrDepartment.java

--- Assignment/src/com/gradedproject/q1/HrDepartment.java
+++ Assignment/src/com/gradedproject/q1/HrDepartment.java
@@ -3,22 +3,26 @@ package com.gradedproject.q1;
 public class HrDepartment extends SuperDepartment {
 	//String res = super.isTodayAHoliday();
 
-	public String departmentName() {
+	public String departmentName() 
+	{
 		String str = "HR Department";
 		return str;
 	}
 
-	public String getTodaysWork() {
+	public String getTodaysWork() 
+	{
 		String str = "Fill today's timesheet and mark your attendance";
 		return str;
 	}
 
-	public String getWorkDeadline() {
+	public String getWorkDeadline() 
+	{
 		String str = "Complete by EOD";
 		return str;
 	}
 
-	public String doActivity() {
+	public String doActivity()
+	{
 		String str = "team Lunch";
 		return str;
 	}
